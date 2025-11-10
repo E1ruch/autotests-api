ba0f1f3da9eeec6cad8f01909a963c5d66167611
VladislavQA <vladbezugloff@yandex.ru>
2025-11-10 19:50:30 +0300

    Знакомство с API клиентами

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -6,28 +6,23 @@ from typing import TypedDict
 class CreateUserRequest(TypedDict):
     """
     Структура данных для создания пользователя через API
-
-    Attributes:
-        username (str): уникальное имя пользователя
-        email (str): электронная почта пользователя
-        password (str): пароль пользователя
     """
-    username: str
     email: str
     password: str
+    lastName: str
+    firstName: str
+    middleName: str
 
 
 class PublicUsersClient(APIClient):
+    """
+       Клиент для работы с /api/v1/users
+       """
     def create_user_api(self, request: CreateUserRequest) -> Response:
         """
-        Создание нового пользователя через POST /api/v1/users.
+        Метод создает пользователя.
 
-        Args:
-            request (CreateUserRequest): Данные пользователя для создания.
-                Содержит username, email и password.
-
-        Returns:
-            Response: Объект httpx.Response, содержащий ответ сервера.
+        :param request: Словарь с email, password, lastName, firstName, middleName.
+        :return: Ответ от сервера в виде объекта httpx.Response
         """
-        response = self.post("/api/v1/users", json=request)
-        return response
+        return self.post("/api/v1/users", json=request)
